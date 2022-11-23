7ebefb4e1908d8a39ea1b30ed642ca9a6fffc445
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-23 12:41:47 +0100

    Removing google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import React, { useEffect } from "react";
 
-// Analytics
-import ReactGA from "react-ga";
-
 // Routing
 import { Routes, Route } from "react-router-dom";
-
-// Tracking ID
-const TRACKING_ID = "UA-188085571-1";
-ReactGA.initialize(TRACKING_ID);
 
 // Pages
 import { Home, Products, Contact, About, NotFound } from "./pages";
 
 const App = () => {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <>
       <Routes>
