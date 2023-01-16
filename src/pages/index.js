be9a03c808789e0c2b3894f4c837dfdc629fab4d
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-01-16 13:25:14 +0100

    Pushing home changes...

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,20 +39,20 @@ const Home = () => {
         <motion.div className="flex items-center justify-center h-screen px-6 sm:px-0">
           {/* Content container */}
           <div>
-            <h1 className="mb-3 text-lg text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-5xl">
+            <h1 className="mb-3 text-2xl text-center sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
               Chers visiteurs, vous êtes les bienvenus
               <br />
               Chez{" "}
               <span className="font-medium gradient-text">ROTIN NIAJAD</span>
             </h1>
 
-            <p className="mb-10 text-center sm:text-[18px] md:text-xl lg:text-2xl">
+            <p className="mb-10 text-lg text-center sm:text-xl md:text-2xl lg:text-3xl">
               Le meilleur endroit pour acheter les meubles de maison.
             </p>
 
             <div className="text-center">
               <Link
-                className="px-8 py-4 rounded-full text-white duration-300 gradient-box text-[18px] md:text-xl lg:text-2xl"
+                className="px-8 py-4 text-lg text-white duration-300 rounded-full shadow-lg gradient-box md:text-xl lg:text-2xl shadow-purple-600/50"
                 href="/products"
               >
                 découvrir nos produits
@@ -71,7 +71,7 @@ const Home = () => {
         >
           {/* Content container */}
           <div className="py-20">
-            <h2 className="mb-8 text-xl font-semibold text-center text-white sm:mb-12 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+            <h2 className="mb-8 text-2xl font-semibold text-center text-white sm:mb-12 sm:text-3xl md:text-4xl lg:text-5xl">
               Nos Services
             </h2>
 
@@ -79,13 +79,13 @@ const Home = () => {
             <div className="grid gap-16 px-6 place-items-center lg:px-0 sm:grid-cols-2 md:grid-cols-3">
               <div className="text-center">
                 <div className="flex items-center justify-center mb-3">
-                  <ShieldCheckIcon className="w-12 h-12 text-white sm:w-14 sm:h-14 lg:w-16 lg:h-16" />
+                  <ShieldCheckIcon className="text-white w-14 h-14 sm:w-16 sm:h-16 lg:w-18 lg:h-18" />
                 </div>
                 <div>
-                  <h3 className="text-lg font-medium text-white sm:text-xl lg:text-2xl">
+                  <h3 className="mb-1 text-xl font-medium text-white sm:text-2xl lg:text-3xl">
                     Excellente qualité
                   </h3>
-                  <p className="text-gray-300 sm:text-lg lg:text-xl">
+                  <p className="text-lg text-gray-300 sm:text-xl lg:text-2xl">
                     On a des produits de bon qualité
                   </p>
                 </div>
@@ -93,28 +93,28 @@ const Home = () => {
 
               <div className="text-center">
                 <div className="flex items-center justify-center mb-3">
-                  <ClockIcon className="w-12 h-12 text-white sm:w-14 sm:h-14 lg:w-16 lg:h-16" />
+                  <ClockIcon className="text-white w-14 h-14 sm:w-16 sm:h-16 lg:w-18 lg:h-18" />
                 </div>
                 <div>
-                  <h3 className="text-lg font-medium text-white sm:text-xl lg:text-2xl">
-                    Respectez les délais
+                  <h3 className="mb-1 text-xl font-medium text-white sm:text-2xl lg:text-3xl">
+                    Respectons les délais
                   </h3>
-                  <p className="text-gray-300 sm:text-lg lg:text-xl">
-                    Respectez les délais
+                  <p className="text-lg text-gray-300 sm:text-xl lg:text-2xl">
+                    On Respecte les délais
                   </p>
                 </div>
               </div>
 
               <div className="text-center">
                 <div className="flex items-center justify-center mb-3">
-                  <TruckIcon className="w-12 h-12 text-white sm:w-14 sm:h-14 lg:w-16 lg:h-16" />
+                  <TruckIcon className="text-white w-14 h-14 sm:w-16 sm:h-16 lg:w-18 lg:h-18" />
                 </div>
                 <div>
-                  <h3 className="text-lg font-medium text-white sm:text-xl lg:text-2xl">
+                  <h3 className="mb-1 text-xl font-medium text-white sm:text-2xl lg:text-3xl">
                     Livraison
                   </h3>
-                  <p className="text-gray-300 sm:text-lg lg:text-xl">
-                    Nous pouvons livrer partout au Maroc
+                  <p className="text-lg text-gray-300 sm:text-xl lg:text-2xl">
+                    nous livraisons partout au Maroc
                   </p>
                 </div>
               </div>
@@ -129,18 +129,18 @@ const Home = () => {
         <motion.div>
           {/* Content container */}
           <div className="pt-20 pb-10">
-            <h2 className="mb-8 text-xl font-semibold text-center sm:mb-12 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+            <h2 className="mb-8 text-2xl font-semibold text-center sm:mb-12 sm:text-3xl md:text-4xl lg:text-5xl">
               Nos Produits
             </h2>
 
             {/* Products container */}
-            <div className="grid gap-12 mb-12 sm:mb-16 place-items-center sm:grid-cols-2 sm:px-12 md:grid-cols-3 lg:grid-cols-4">
+            <div className="grid gap-6 mb-12 sm:gap-12 sm:mb-16 place-items-center sm:grid-cols-2 sm:px-12 md:grid-cols-3 lg:grid-cols-4">
               {[1, 2, 3, 4].map((image, index) => (
                 <div key={index} className="w-[300px] h-[325px]">
                   <Image
                     className="object-contain w-full h-full border-none gradient-box"
                     src={image}
-                    alt={""}
+                    alt="featured product"
                   />
                 </div>
               ))}
