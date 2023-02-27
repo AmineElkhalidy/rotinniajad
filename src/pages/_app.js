c9037df5c9469a874e819ae262d73a1d928b841b
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-02-27 20:30:06 +0100

    Adding GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 // Head
 import Head from "next/head";
+import Script from "next/script";
 
 // Layout
 import { Layout } from "@/components";
@@ -17,6 +18,25 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" type="image/svg+xml" href="./couch.svg" />
       </Head>
 
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-WHDWB1486G"
+      ></Script>
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WHDWB1486G', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
